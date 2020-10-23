64604e7e16d9131ccf0f94cd71645ec61b42c5c2
bkilimni <benjamin_kilimnik@brown.edu>
2020-10-23 16:20:59 -0400

    ready for deployment

--- start.js
+++ start.js
@@ -1,16 +1,16 @@
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
 
-// app.use(express.static('./public')); //Serves resources from public folder
-// var server = app.listen(5000)
+app.use(express.static('./public')); //Serves resources from public folder
+var server = app.listen(5000)
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
