55abf9edd5de6a5099ad997a1d00e00cc63c6da6
bkilimni <benjamin_kilimnik@brown.edu>
2020-10-23 15:56:31 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
